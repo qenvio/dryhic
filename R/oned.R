ba62446a2bac1b7706395de4452c7ff753b40176
qenvio <enrique.vidal@crg.eu>
2017-02-06 13:40:17 +0100

    import nb in oned

--- R/oned.R
+++ R/oned.R
@@ -2,6 +2,7 @@
 #'
 #' This function takes a \code{data.frame} with bin information and returns a vector of  biases to correct for
 #' @importFrom mgcv gam
+#' @importFrom mgcv nb
 #' @param dat A \code{data.frame} with one bin per row containing the total number of contacts and the potential biases as columns.
 #' @param form A \code{formula} describing the  total number of contacts on LHS and the smoothed biases on the RHS (should be compatible with \code{\link{mgcv::gam}})
 #' @return A vector of length \code{nrow(dat)} with the biases to correct for.
