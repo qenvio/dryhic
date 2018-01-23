1f41754e448e7d4cb96fbe989e14e490b29055aa
qenvio <enrique.vidal@crg.eu>
2018-01-23 17:08:10 +0100

    Forcing eigenvectors to be Real (now in the right place)

--- R/reproducibility_score.R
+++ R/reproducibility_score.R
@@ -59,13 +59,13 @@ reproducibility_score <- function(mat1, mat2, r = 20, mipr = 5){
     
     r <- min(r, nrow(l1)) %>% min(nrow(l2))
 
-    ei1 <- RSpectra::eigs(l1, r, which = "SM") %>% Re
-    ei2 <- RSpectra::eigs(l2, r, which = "SM") %>% Re
+    ei1 <- RSpectra::eigs(l1, r, which = "SM")
+    ei2 <- RSpectra::eigs(l2, r, which = "SM")
    
     ev1 <- matrix(0, nrow(l1), r)
-    ev1 <- ei1$vectors[, 1:r]
+    ev1 <- Re(ei1$vectors[, 1:r])
     ev2 <- matrix(0, nrow(l2), r)
-    ev2 <- ei2$vectors[, 1:r]
+    ev2 <- Re(ei2$vectors[, 1:r])
 
     ipr1 <- 1 / colSums(ev1 ^ 4)
     ipr2 <- 1 / colSums(ev2 ^ 4)
