9a396bdd9eec8701508b60336abe53f97a6f09da
qenvio <enrique.vidal@crg.eu>
2018-08-08 23:19:49 +0200

    scc is now more proficent, thanks to data.table

--- R/scc.R
+++ R/scc.R
@@ -1,6 +1,8 @@
 #' Compute the stratum adjusted correlation coefficient (SCC) between two contact matrices.
 #'
 #' This function takes a couple of contact matrices and computes the reproducibility score.
+#' @import magrittr
+#' @import data.table
 #' @param mat1 A \code{dgTMatrix} of contacts with genomic bins as \code{row.names}.
 #' @param mat2 Another \code{dgTMatrix} of contacts with genomic bins as \code{row.names}.
 #' @param mm The maximum distance (in bp) to truncate the data.
@@ -13,34 +15,76 @@
 #' plot(0)
 
 scc <- function(mat1, mat2, mm, resol){
-
-    unitize <- function(x){
-        rank(x, ties.method = "random") / length(x)
+    unitize <- function(x) {
+        rank(x, ties.method = "random")/length(x)
     }
+    if (!(class(smat1) == "dgTMatrix" | class(smat1) == "dgCMatrix")) 
+        stop("smat1 should be a sparse matrix")
+    if (!(class(smat2) == "dgTMatrix" | class(smat2) == "dgCMatrix")) 
+        stop("smat2 should be a sparse matrix")
 
-    dat <- paste_matrices(mat1, mat2, mm) %>% na.omit
+    out1 <- mat2df(smat1) %>% as.data.frame
+    p1 <- rownames(smat1) %>% gsub("^.*:", "", .) %>% as.numeric
+    out1 <- mutate(out1, x1 = x, x = NULL) %>%
+        filter(j - i <= mm, i <= j)
+
+    out2 <- mat2df(smat2) %>% as.data.frame
+    p2 <- rownames(smat2) %>% gsub("^.*:", "", .) %>% as.numeric
+    out2 <- mutate(out2, x2 = x, x = NULL) %>%
+        filter(j - i <= mm, i <= j)
+
+    pb <- intersect(p1, p2)
+
+    aux <- bandSparse(nrow(smat1), ncol(smat1),
+                      k = 0:(mm / resol),
+                      diagonals = matrix(1,
+                                         nrow(smat1),
+                                         mm / resol + 1)) %>%
+        as(class(smat1))
+    rownames(aux) <- colnames(aux) <- rownames(smat1)
+
+    scaf <- mat2df(aux) %>%
+        filter(i %in% pb, j %in% pb) %>%
+        dplyr::select(-x)
+
+    out1 <- data.table(out1)
+    out2 <- data.table(out2)
+    scaf <- data.table(scaf)
+
+    setkey(out1, i, j)
+    setkey(out2, i, j)
+    setkey(scaf, i, j)
+
+    out <- out1[out2[scaf]]
+
+    out[is.na(x1), x1 := 0]
+    out[is.na(x2), x2 := 0]
+
+    out[, d:= abs(i - j)/resol]
     
-    if(min(unique(dat$x1) %>% length,
-           unique(dat$x2) %>% length) == 1) {
-        
+    if (min(unique(out$x1) %>% length, unique(out$x2) %>% length) == 
+        1) {
         return(list(scc = NA, std = NA))
-        
     }
-    
-    res <- mutate(dat, d = abs(i - j) / resol) %>%
-        dplyr::group_by(d) %>%
-        mutate(r1 = unitize(x1),
-               r2 = unitize(x2)) %>%
-        summarize(corr = cor(x1, x2),
-                  wei = sqrt(var(r2) * var(r1)) * n()) %>%
-        dplyr::ungroup() %>%
-        dplyr::select(-d) %>%
-        na.omit %>%
-        as.list
 
-    res$scc <- with(res, corr * wei/sum(wei)) %>% as.numeric %>% sum
-    res$std <- with(res, sqrt(sum(wei^2 * var(corr))/(sum(wei))^2)) %>% as.numeric
+    out <- out[, .(r1 = unitize(x1),
+                   r2 = unitize(x2),
+                   x1,
+                   x2),
+               .(d)]
+    out <- out[, .(corr = cor(x1, x2),
+                   wei = sqrt(var(r2) * var(r1)) * .N),
+               .(d)]
 
-    res
+    res <- as.list(out)
+    res$scc <- with(res,
+                    corr * wei/sum(wei)) %>%
+        as.numeric() %>% 
+        sum()
+    res$std <- with(res, (sum(wei^2 * var(corr))/(sum(wei))^2) %>%
+                         sqrt()) %>% 
+        as.numeric()
+
+    res[c("corr", "wei", "scc", "std")]
 
 }
